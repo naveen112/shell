7fd5a6ab07dc50df7557ed8d334f775098f770fa
naveen112 <39689116+naveen112@users.noreply.github.com>
2019-03-11 15:19:13 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 apt-get install python
 cd /boardfarm/tests
 for f in *.py; do python "$f"; done
-apt-get install jmeter
+apt-get install jmeter -y
 cd /boardfarm/tests/jmeter
 for f in *.jmx; do  jmeter -n -t "$f"; done
 cd /boardfarm/tests/lib
