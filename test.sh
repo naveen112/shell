87c67aa91fee136327227747dfd88affe59eef7d
naveen112 <39689116+naveen112@users.noreply.github.com>
2019-03-11 14:53:45 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+!/bin/sh
+apt-get install python
+cd /boardfarm/tests
+for f in *.py; do python "$f"; done
+apt-get install jmeter
+cd /boardfarm/tests/jmeter
+for f in *.jmx; do  jmeter -n -t "$f"; done
+cd /boardfarm/tests/lib
+for f in *.py; do python "$f"; done
+apt-get install tcl8.4
+tclsh mta_conf_Proc.tcl
+cd /boardfarm/tests/lib/scripts
+sh openvpn-install.sh
